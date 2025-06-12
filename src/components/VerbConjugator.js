2ac6ed72d5462a4ae4a27a5d2f7adcb2e2e476e4
Ehsan <ej.jamshidi@gmail.com>
2025-06-12 12:11:50 +0800

    Add exact match flag to anime examples link

--- src/components/VerbConjugator.js
+++ src/components/VerbConjugator.js
@@ -247,7 +247,7 @@ export default function VerbConjugator() {
                 href={`https://immersion-dictionary-r2saexnz6-mathew-chans-projects-22577ade.vercel.app/dictionary?keyword=${encodeURIComponent(
                   (result.conjugations.find(c => c.conjugation === 'Non-past Affirmative')?.polite ||
                     result.title.replace(' Conjugation Table', ''))
-                )}&sort=sentence_length%3Aasc`}
+                )}&exact=true&sort=sentence_length%3Aasc`}
                 target="_blank"
                 rel="noopener noreferrer"
               >
